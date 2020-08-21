4443bed4465fb028cf539a2c5254ca41ab8ee003
Ariel Cuesta <arielcc88@gmail.com>
2020-08-20 22:33:24 -0500

    initial commit of burger app. defining schema, seeders and installling node pkgs.

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Seeding queries reference with burger options
+USE BURGERS_DB;
+
+-- Seeding burgers table
+INSERT INTO `burgers` VALUES
+('1','Whataburger', false),
+('2','Double Meat Whataburger', false),
+('3','Bacon & Cheese Whataburger', true),
+('4','Jalapeno & Cheese Whataburger', false);
