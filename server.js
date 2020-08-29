06f26d6b4e0a6fde639c8e6a8a012c40f8627c57
Ariel Cuesta <arielcc88@gmail.com>
2020-08-28 20:13:44 -0500

    server.js created and tested.

--- server.js
+++ server.js
@@ -7,3 +7,31 @@
  const PORT = process.env.PORT || 3000;
 
  //express app
+ const app = express();
+
+ //body parser as JSON
+ app.use(express.urlencoded({ extended: true }));
+ app.use(express.json());
+
+ //Static middleware to serve public content
+ app.use(express.static("public"));
+
+ //importing handlebars as templating engine
+ const exphbs = require("express-handlebars");
+ //handlebars settings
+ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+ app.set("view engine", "handlebars");
+
+ //importing routes and set the app to use them
+ const appRoutes = require("./controllers/burgers_controllers.js");
+ app.use(appRoutes);
+
+
+ //starting the server and listening at port
+ app.listen(PORT, () => {
+    console.log("Server started and listening on PORT: " + PORT);
+ });
+
+
+
+
