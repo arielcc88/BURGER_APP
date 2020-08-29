9b3085e7f46e2d2d08061b01121ac4e173eecc25
Ariel Cuesta <arielcc88@gmail.com>
2020-08-29 18:08:34 -0500

    modifying connection to accomodate JAWSDB for Heorku deployment.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 //MYSQL instance for connection
 const mysql = require("mysql");
-
+let connection;
 //connection settings
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootpass",
-    database:"BURGERS_DB"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "",
+        password: "",
+        database:"BURGERS_DB"
+    });
+}
 
 //making connection
 connection.connect((err) => {
